00cd99d107c4f1f43216d22bb8909488246643fa
AmeriCodes <mateus-americo@live.com>
2023-09-28 17:35:27 -0300

    feat: Chord command implementation in CLI

--- harmony_hub/cli.py
+++ harmony_hub/cli.py
@@ -2,19 +2,36 @@ from rich.console import Console
 from rich.table import Table
 from typer import Argument, Typer
 
-from harmony_hub.scales import scale
+from harmony_hub.chords import chord as _chord
+from harmony_hub.scales import scale as _scale
 
 console = Console()
 app = Typer()
 
 
 @app.command()
-def scales(
+def scale(
     tonic: str = Argument('c', help='Tonic of the scale'),
     mode: str = Argument('major', help='Scale character'),
 ):
     table = Table()
-    notes, degrees = scale(tonic, mode).values()
+    notes, degrees = _scale(tonic, mode).values()
+
+    for degree in degrees:
+        table.add_column(degree)
+
+    table.add_row(*notes)
+
+    console.print(table)
+
+
+@app.command()
+def chord(
+    cipher: str = Argument('C', help='Chord notation'),
+):
+    table = Table()
+
+    notes, degrees = _chord(cipher).values()
 
     for degree in degrees:
         table.add_column(degree)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,23 +7,35 @@ runner = CliRunner()
 
 
 def test_scale_cli_must_return_0_to_stdout():
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['scale'])
     assert result.exit_code == 0
 
 
 @mark.parametrize('note', ['C', 'D', 'E', 'F', 'G', 'A', 'B'])
 def test_cli_scale_must_contain_the_notes_in_the_response(note):
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['scale'])
     assert note in result.stdout
 
 
 @mark.parametrize('note', ['F', 'G', 'A', 'Bb', 'C', 'D', 'E'])
 def test_cli_scale_must_contain_the_notes_in_the_f_response_scale(note):
-    result = runner.invoke(app, ['F'])
+    result = runner.invoke(app, ['scale', 'F'])
     assert note in result.stdout
 
 
 @mark.parametrize('degree', ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'])
 def test_cli_scale_must_contain_the_all_degrees(degree):
-    result = runner.invoke(app, ['F'])
+    result = runner.invoke(app, ['scale', 'F'])
+    assert degree in result.stdout
+
+
+@mark.parametrize('note', ['C', 'E', 'G'])
+def test_cli_chords_must_contain_the_notes_in_the_response(note):
+    result = runner.invoke(app, ['chord'])
+    assert note in result.stdout
+
+
+@mark.parametrize('degree', ['I', 'III', 'V'])
+def test_cli_chords_must_contain_the_notes_in_the_response(degree):
+    result = runner.invoke(app, ['chord'])
     assert degree in result.stdout
